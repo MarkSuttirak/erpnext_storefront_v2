145fb1021216e99a4c891c10e45e5a2e6afe6163
MarkSuttirak <suttirak.ch@zaviago.com>
2023-10-16 18:17:55 +0700

    Commit Codes

--- src/components/desktop/HeaderDesktop.jsx
+++ src/components/desktop/HeaderDesktop.jsx
@@ -7,7 +7,7 @@ import { useFrappeAuth } from 'frappe-react-sdk';
 import { useCart } from '../../hooks/useCart';
 import { useNavigate } from 'react-router-dom';
 import { Fragment, useState, useRef, useEffect } from 'react'
-import { ShoppingBag01, MessageCircle01, ChevronDown, Heart } from "@untitled-ui/icons-react";
+import { ShoppingBag01, MessageCircle01, ChevronDown, Heart, SearchMd } from "@untitled-ui/icons-react";
 import { useFrappeGetDocList } from 'frappe-react-sdk';
 
 const HeaderDesktop = () => {
@@ -37,61 +37,75 @@ const HeaderDesktop = () => {
 
     return (
       <header className="hidden lg:flex lg:flex-col justify-center w-full z-[999] fixed top-0 bg-white">
-          <div className='flex bg-[#F2F2F2] justify-between px-6 py-2'>
-            <div className='max-w-[1200px] mx-auto w-full flex justify-between'>
-              <p className='text-[#424242] text-sm'>12.12 โปรโมชั่นทั้งเว็บไซต์</p>
-              <div className='flex gap-x-2'>
-                <button className='text-[#424242] text-sm'>หน้าร้านของเรา</button>
-                <button className='text-[#424242] text-sm'>ติดต่อร้านค้า</button>
-                <button className='text-[#424242] text-sm'>ลงชื่อเข้าใช้</button>
-              </div>
+        <div className='flex bg-[#F2F2F2] justify-between px-6 py-2'>
+          <div className='max-w-[1200px] mx-auto w-full flex justify-between'>
+            <p className='text-[#424242] text-sm'>12.12 โปรโมชั่นทั้งเว็บไซต์</p>
+            <div className='flex gap-x-2'>
+              <button className='text-[#424242] text-sm'>หน้าร้านของเรา</button>
+              <div className='border-l border-l-[#424242] w-[1px] h-full' />
+              <button className='text-[#424242] text-sm'>ติดต่อร้านค้า</button>
+              <div className='border-l border-l-[#424242] w-[1px] h-full' />
+              <button className='text-[#424242] text-sm'>ลงชื่อเข้าใช้</button>
             </div>
           </div>
-          <div className="flex flex-wrap lg:flex-nowrap items-center flex-row h-full w-full py-2 px-4">
-            <div className='max-w-[1200px] mx-auto flex items-center w-full'>
-              <button className='flex flex-1 items-center gap-x-[6px]'>
-                หมวดหมู่
-                <ChevronDown />
-              </button>
-              <picture>
-                <a
-                  href="/"
-                  aria-label="SF Homepage"
-                  className="flex-1 justify-center flex focus-visible:outline focus-visible:outline-offset focus-visible:rounded-sm shrink-0"
-                >
-                  <source srcSet={brandLogo} media="(min-width: 768px)" />
-                  <img
-                    src={brandLogo}
-                    alt="Sf Logo"
-                    className="w-[100px]"
-                  />
-                </a>
-              </picture>
+        </div>
+        <div className="flex flex-wrap lg:flex-nowrap items-center flex-row h-full w-full py-2 px-4">
+          <div className='max-w-[1200px] mx-auto flex items-center w-full'>
+            <button className='flex flex-1 items-center gap-x-[6px]'>
+              หมวดหมู่
+              <ChevronDown />
+            </button>
+            <picture>
+              <a
+                href="/"
+                aria-label="SF Homepage"
+                className="flex-1 justify-center flex focus-visible:outline focus-visible:outline-offset focus-visible:rounded-sm shrink-0"
+              >
+                <source srcSet={brandLogo} media="(min-width: 768px)" />
+                <img
+                  src={brandLogo}
+                  alt="Sf Logo"
+                  className="w-[100px]"
+                />
+              </a>
+            </picture>
 
-              <nav className="flex-1 flex justify-end order-last lg:ml-4">
-                <div className="flex flex-row flex-nowrap">
-                    {actionItems.map((actionItem) => (
-                        <SfButton
-                          key={actionItem.ariaLabel}
-                          className="relative ml-1 rounded-[99px] hover:bg-white"
-                          aria-label={actionItem.ariaLabel}
-                          variant="tertiary"
-                          square
-                          slotPrefix={actionItem.icon}
-                          onClick={actionItem.onClick}
-                        >
-                          {actionItem.ariaLabel === 'Cart' && (
-                            <SfBadge content={cartCount} />
-                          )}
-                          {actionItem.role === 'login' && (
-                            <p className="hidden xl:inline-flex whitespace-nowrap">{actionItem.label}</p>
-                          )}
-                        </SfButton>
-                    ))}
-                  </div>
-              </nav>
-            </div>
+            <nav className="flex-1 flex justify-end order-last lg:ml-4">
+              <div className="relative mt-1 rounded-md">
+                <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                  <SearchMd color='#424242'/>
+                </div>
+                <input
+                  type="text"
+                  name="search"
+                  id="search"
+                  className="block w-full rounded-md pl-11 sm:text-sm h-full bg-[#F4F4F4] outline-none"
+                  placeholder="ค้นหา"
+                />
+              </div>
+              <div className="flex flex-row flex-nowrap">
+                  {actionItems.map((actionItem) => (
+                      <SfButton
+                        key={actionItem.ariaLabel}
+                        className="relative ml-1 rounded-[99px] hover:bg-white"
+                        aria-label={actionItem.ariaLabel}
+                        variant="tertiary"
+                        square
+                        slotPrefix={actionItem.icon}
+                        onClick={actionItem.onClick}
+                      >
+                        {actionItem.ariaLabel === 'Cart' && (
+                          <SfBadge content={cartCount} />
+                        )}
+                        {actionItem.role === 'login' && (
+                          <p className="hidden xl:inline-flex whitespace-nowrap">{actionItem.label}</p>
+                        )}
+                      </SfButton>
+                  ))}
+                </div>
+            </nav>
           </div>
+        </div>
       </header>
     )
 }
--- src/pages/MyOrder.jsx
+++ src/pages/MyOrder.jsx
@@ -1,13 +1,40 @@
 import React, { useState, useRef } from "react"
 import { ArrowLeft } from "@untitled-ui/icons-react"
 import { useFrappeGetDocList, useFrappeGetCall } from "frappe-react-sdk"
-import testImg from '../img/test-img.png'
 import { Link, useParams } from "react-router-dom"
 import { getToken, removeToken, setToken } from '../utils/helper';
+import testImg from '../img/test-img.png'
 
 const MyOrder = () => {
   const { id } = useParams();
   const [myorderlist, setmyorderlist] = useState()
+
+  const mockOrders = [
+    {
+      name:"Computer",
+      posting_date:"12. Juli 2023",
+      status:"Unbezahlt",
+      total:"€60"
+    },
+    {
+      name:"Salat",
+      posting_date:"12. August 2023",
+      status:"Aktiv",
+      total:"€5"
+    },
+    {
+      name:"Handy",
+      posting_date:"25. August 2023",
+      status:"Unbezahlt",
+      total:"€150"
+    },
+    {
+      name:"Sandwich",
+      posting_date:"10. August 2023",
+      status:"Entwurf",
+      total:"€3"
+    },
+  ]
  
   const { data, isLoading, error } = useFrappeGetDocList('Sales Invoice', {
     fields: ['name', 'posting_date', 'status', 'total'],
@@ -36,26 +63,30 @@ const MyOrder = () => {
         รายละเอียดการแลกของรางวัล
       </header>
       <main className="p-5 flex flex-col gap-y-[18px] mt-[53px]">
-        {(myorderlist ?? []).map((d) => 
+        {mockOrders.map((d) => 
           <Link to={`/my-order-details/${d.name}`}>
-            <section className="flex gap-x-[14px] mt-[14px] pb-[18px] border-b border-b-[#E3E3E3]">
-              <div className="flex w-full flex-col gap-y-3">
-                <div className="flex">
-                  <h2 className="w-[40%] text-xs">คำสั่งซื้อ</h2>
-                  <p className="w-[60%] text-xs text-[#00B14F]">{d.name}</p>
+            <section className="mt-[14px] pb-[18px] border-b border-b-[#E3E3E3]">
+              <div className="flex mb-[14px]">
+                <p className={`w-[60%] text-xs`}>
+                  {d.status} {/* Entwurf = Draft */}
+                </p>
+              </div>
+              <div className="flex gap-x-[14px]">
+                <div>
+                  <img src={testImg} />
                 </div>
-                <div className="flex">
-                  <h2 className="w-[40%] text-xs">วันที่</h2>
-                  <p className="w-[60%] text-xs">{d.posting_date}</p>
-                </div>
+                <div className="flex w-full flex-col gap-y-3">
                   <div className="flex">
-                  <h2 className="w-[40%] text-xs">Status</h2>
-                  <p className={`w-[60%] text-xs`}>
-                    {d.delivery_notes[0]?.status === 'Draft' ? 'สามารถใช้คูปองได้' : d.delivery_notes[0]?.status === 'Submitted' ? 'คูปองถูกใช้แล้ว' : ''}
-                  </p>
-                </div>
-                <div className="w-full">
-                  <Link to={d.delivery_notes[0]?.status === 'Draft' && `/my-order-details/${d.name}`} className='w-full block text-white rounded-[9px] p-3 text-center' style={{background:d.delivery_notes[0]?.status === 'Draft' ? "#111111" : "#C5C5C5"}}>ดูข้อมูล</Link>
+                    <h2 className="w-[40%] text-xs">คำสั่งซื้อ</h2>
+                    <p className="w-[60%] text-xs text-[#00B14F]">{d.name}</p>
+                  </div>
+                  <div className="flex">
+                    <h2 className="w-[40%] text-xs">วันที่</h2>
+                    <p className="w-[60%] text-xs">{d.posting_date}</p>
+                  </div>
+                  <div className="w-full">
+                    <Link to={d.status === 'Draft' && `/my-order-details/${d.name}`} className='w-full block text-white rounded-[9px] p-3 text-center' style={{background:d.status === 'Draft' ? "#111111" : "#C5C5C5"}}>ดูข้อมูล</Link>
+                  </div>
                 </div>
               </div>
             </section>
